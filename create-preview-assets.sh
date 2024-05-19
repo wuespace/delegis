72be3a3c7bf4ee483bdbf8cd5de3fe5524f9b725
Zuri Klaschka <pklaschka@users.noreply.github.com>
2024-05-19 14:58:00 +0200

    Update preview assets

--- create-preview-assets.sh
+++ create-preview-assets.sh
@@ -41,10 +41,10 @@ done
 cp demo-3.png thumbnail.png
 
 # Commit and push changes
+git add .
 if git diff-index --quiet HEAD --; then
 	echo "No changes to commit"
 else
-	git add .
 	git commit -m "Update preview assets"
 	git push
 	echo "Changes were committed and pushed"
